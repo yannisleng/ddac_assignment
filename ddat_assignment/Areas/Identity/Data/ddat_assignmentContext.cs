733b0e69f6408d72f3f0cfaf69d984e924ef97f4
yannisleng <lengzhiyan1015@gmail.com>
2024-06-07 17:47:43 +0800

    feat: added login (followed tutorial)

---
+++ ddat_assignment/Areas/Identity/Data/ddat_assignmentContext.cs
@@ -0,0 +1,22 @@
+﻿using ddat_assignment.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ddat_assignment.Data;
+
+public class ddat_assignmentContext : IdentityDbContext<ddat_assignmentUser>
+{
+    public ddat_assignmentContext(DbContextOptions<ddat_assignmentContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
--- ddat_assignment/Program.cs
+++ ddat_assignment/Program.cs
@@ -1,7 +1,17 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ddat_assignment.Data;
+using ddat_assignment.Areas.Identity.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("ddat_assignmentContextConnection") ?? throw new InvalidOperationException("Connection string 'ddat_assignmentContextConnection' not found.");
+
+builder.Services.AddDbContext<ddat_assignmentContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<ddat_assignmentUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ddat_assignmentContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
@@ -14,10 +24,14 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
 
 app.Run();
