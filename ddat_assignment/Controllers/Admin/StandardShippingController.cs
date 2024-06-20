64ceefe02e2dbb9fcb567e69dbd373756980209e
KaiKiat <92348692+Kaikiat1126@users.noreply.github.com>
2024-06-21 01:18:14 +0800

    feat: add admin standard shipping page

---
+++ ddat_assignment/Controllers/Admin/StandardShippingController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ddat_assignment.Models;
+
+namespace ddat_assignment.Controllers.Admin
+{
+    public class StandardShippingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /* TODO */
+        [HttpPost]
+        public async Task<IActionResult> CreateModels(FormCollection fc)
+        {
+            Console.WriteLine("CreateModels called");
+            Console.WriteLine("FormCollection: " + fc);
+            //get the form data
+            /*var shipmentModel = new ShipmentModel
+            {
+                ShipmentId = Guid.NewGuid(),
+            };*/
+            //create the parcel model & shipping model & payment model
+            //save the models
+            return View();
+        }
+    }
+}
---
+++ ddat_assignment/Controllers/Admin/WorkspaceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ddat_assignment.Controllers.Admin
+{
+    public class WorkspaceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
