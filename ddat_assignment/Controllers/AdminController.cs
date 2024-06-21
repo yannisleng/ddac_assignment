8f9952f1f8aa32f902bd5650d9558ed44dddd62c
KaiKiat <92348692+Kaikiat1126@users.noreply.github.com>
2024-06-21 17:47:51 +0800

    feat: sidebar styling and rendering shipment data

--- ddat_assignment/Controllers/AdminController.cs
+++ ddat_assignment/Controllers/AdminController.cs
@@ -1,14 +1,33 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using ddat_assignment.Models;
+using ddat_assignment.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
 
 namespace ddat_assignment.Controllers
 {
     [Authorize(Roles = "Warehouse")]
     public class AdminController : Controller
     {
-        public IActionResult Index()
+        private readonly ddat_assignmentContext _context;
+
+        public AdminController(ddat_assignmentContext context)
         {
-            return View();
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<ShipmentModel> shipments = await _context.ShipmentModel.ToListAsync();
+            //for each shipments, the address property replace the "||" with ","
+            foreach (ShipmentModel shipment in shipments)
+            {
+                shipment.PickupAddress = shipment.PickupAddress.Replace("||", ",");
+                shipment.DeliveryAddress = shipment.DeliveryAddress.Replace("||", ",");
+                shipment.Parcel = await _context.ParcelModel.FirstOrDefaultAsync(p => p.ParcelId == shipment.ParcelId);
+            }
+            shipments = shipments.OrderBy(s => s.ShipmentDate).ToList();
+            return View(shipments);
         }
 
         public IActionResult Workspace()
@@ -17,6 +36,11 @@ namespace ddat_assignment.Controllers
         }
 
         public IActionResult StandardShipping()
+        {
+            return View();
+        }
+
+        public IActionResult ManageDrivers()
         {
             return View();
         }
