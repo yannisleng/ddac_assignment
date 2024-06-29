fdac499e00f52ee67994817296ae6508afd60d25
KaiKiat <92348692+Kaikiat1126@users.noreply.github.com>
2024-06-30 02:47:39 +0800

    feat: shipment transition rendering done

--- ddat_assignment/Controllers/AdminController.cs
+++ ddat_assignment/Controllers/AdminController.cs
@@ -105,9 +105,17 @@ namespace ddat_assignment.Controllers
                 TempData["error"] = "Invalid Shipment ID!";
                 return View();
             }
-            ShipmentModel shipment = SearchShipment(searchQueryUuid).Result;
-            if (shipment == null) TempData["error"] = "Shipment with Shipment Id: " + searchQuery + " not found!";
-            return View(shipment);
+            ShipmentResultModel shipmentResultModel = new ShipmentResultModel
+            {
+                Shipment = SearchShipment(searchQueryUuid).Result,
+                Transitions = GetShipmentTransitions(searchQueryUuid).Result
+            };
+            if (shipmentResultModel.Shipment == null)
+            {
+                TempData["error"] = "Shipment with Shipment Id: " + searchQuery + " not found!";
+                return View();
+            }
+            return View(shipmentResultModel);
         }
 
         public async Task<ShipmentModel> SearchShipment(Guid shipmentId)
@@ -120,6 +128,14 @@ namespace ddat_assignment.Controllers
                 return shipment;
             }
             return null;
+        }
+
+        public async Task<List<TransitionModel>> GetShipmentTransitions(Guid shipmentId)
+        {
+            List<TransitionModel> transitions = await _context.TransitionModel.Where(t => t.ShipmentId == shipmentId).ToListAsync();
+            //order the transitions by timestamp in desc order
+            transitions = transitions.OrderByDescending(t => t.Timestamp).ToList();
+            return transitions;
         }
 
         public IActionResult FilterShipmentStatus(string parameter)
